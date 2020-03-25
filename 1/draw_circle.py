398c79490c4d260e60a14938de595470741602cd
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-03-26 00:22:13 +0900

    Add first pygame-practice. drawing circle.

---
+++ 1/draw_circle.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+def draw_circle():
+    # initialize pygame
+    pygame.display.init()
+
+    # set up the drawing window
+    screen = pygame.display.set_mode([250, 250])
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # if the user clicked the close button
+                running = False
+
+        # fill background with white color
+        screen.fill((255, 255, 255))
+
+        # draw solid blue circle in the center
+        # 1: screen, 2: color, 3: position, 4: radius
+        pygame.draw.circle(screen, (0, 0, 255), (125, 125), 75)
+
+        # flip the display
+        pygame.display.flip()
+
+    # pygame.display.quit()
+    pygame.display.quit()
+
+
+if __name__ == '__main__':
+    draw_circle()
